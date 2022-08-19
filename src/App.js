1b6be2e85fe3b6dc82c29f8dae364c4b65b7ad31
amar13mehta <amar13iam@gmail.com>
2022-08-19 19:18:30 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Foot from "./components/Foot";
 import Head from "./components/Head";
 import Home from "./pages/Home";
-import Property from "./pages/Property";
 import React from "react";
-import Search from "./components/Search";
 
 const App = () => {
   return (
